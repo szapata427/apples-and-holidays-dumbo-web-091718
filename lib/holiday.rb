82116d38db66ed51e40aad877527a860b74fc1eb
Steven Zapata <szapata427@gmail.com>
2018-09-10 14:45:17 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,6 +67,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # remember to return the updated hash
   
   holiday_hash << season
+  holiday_hash[season] << holiday_name
 
 end
 
